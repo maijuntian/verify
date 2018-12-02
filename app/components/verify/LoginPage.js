ecfa6b4d0ffdfc43c719a277bc963a6c3b81281f
maijuntian <348450134@qq.com>
2018-12-02 10:46:48 +0800

    test

---
+++ app/components/verify/LoginPage.js
@@ -0,0 +1,27 @@
+/**
+ * Created by mai on 2017/11/10.
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Image, View, Text, StatusBar, TouchableOpacity} from "react-native";
+import styles, {screenHeight, statusHeight} from "../../style";
+
+/**
+ * 主页
+ */
+class LoginPage extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <View style={[styles.mainBox, styles.centerH]}>
+            </View>
+        )
+    }
+}
+
+export default LoginPage
