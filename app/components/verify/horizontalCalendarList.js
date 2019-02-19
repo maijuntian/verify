ef28a8e805b398adf903d42442e0889883ee4648
maijuntian <jtmai@xtremeprog.com>
2019-02-19 18:41:02 +0800

    扫码区域限制

--- app/components/verify/horizontalCalendarList.js
+++ app/components/verify/horizontalCalendarList.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
-import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
-import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
+// import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
+// import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 
 import {CalendarList} from 'react-native-calendars';
 import {View, WebView} from 'react-native';
@@ -9,14 +9,14 @@ import * as constant from "../../style/constant";
 import FBLoginView from "./FBLoginView";
 
 
-GoogleSignin.configure({
+/*GoogleSignin.configure({
     webClientId: '784601142106-oics6l2v158ao4piqm67jpqnmh6vd6er.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
     offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
     hostedDomain: '', // specifies a hosted domain restriction
     loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
     forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
     accountName: '', // [Android] specifies an account name on the device that should be used
-});
+});*/
 
 export default class HorizontalCalendarList extends Component {
     constructor(props) {
@@ -24,7 +24,7 @@ export default class HorizontalCalendarList extends Component {
     }
 
     // Somewhere in your code
-    _signIn = async () => {
+   /* _signIn = async () => {
         try {
             await GoogleSignin.hasPlayServices();
             const userInfo = await GoogleSignin.signIn();
@@ -41,7 +41,7 @@ export default class HorizontalCalendarList extends Component {
                 // some other error happened
             }
         }
-    };
+    };*/
 
     render() {
         return (
@@ -73,11 +73,11 @@ export default class HorizontalCalendarList extends Component {
                     }}
                 />*/}
 
-                <GoogleSigninButton
+                {/*<GoogleSigninButton
                     style={{ width: 192, height: 48 }}
                     size={GoogleSigninButton.Size.Wide}
                     color={GoogleSigninButton.Color.Dark}
-                    onPress={this._signIn} />
+                    onPress={this._signIn} />*/}
             </View>
         );
     }
